dd911eab2c304c5b3ff9448a06e355728708bb99
Chiforiuc Iona Emanuel <bumblebee12367@gmail.com>
2021-03-21 21:50:01 +0200

    Added 2 buttons in SecondActivity( back/exit)

--- app/src/main/java/com/example/tema1/SecondActivity.java
+++ app/src/main/java/com/example/tema1/SecondActivity.java
@@ -2,13 +2,40 @@ package com.example.tema1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button back=(Button) findViewById(R.id.back);
+        Button ext = (Button) findViewById(R.id.exit);
+        ext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
     }
+
+    public void openActivity1(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
